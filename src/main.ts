421fc18eb5d6dd60885f780e04fbee2fadc911de
gvnaresshuser <gvnaressh@gmail.com>
2026-04-05 11:31:20 +0530

    fixed start:prod path again

--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,8 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  //await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
 /*
